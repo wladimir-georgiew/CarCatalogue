effd20b9370330706798e75326b770932634ecd1
Vladimir Georgiev <vladimir.georgiev.dev@gmail.com>
2023-07-08 03:29:29 +0300

    Cleaning up redundant items

--- CarCatalogue/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CarCatalogue.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
